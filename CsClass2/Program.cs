627fe33d8adc0445c569e0967d7671b47e7a44e5
s2021 <s2021@e-mirim.hs.kr>
2022-08-24 08:59:46 +0900

    out 키워드 실습 #3

--- CsClass2/Program.cs
+++ CsClass2/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace CsClass2
 {
+
+
     internal class Program
     {
         static void Main(string[] args)
@@ -21,6 +23,29 @@ namespace CsClass2
             Products p = new Products();
             Console.WriteLine(p[4]);
             p[4] = 5;
+
+            Console.Write("숫자 입력 : ");
+            int output;
+            bool res = int.TryParse(Console.ReadLine(), out output);
+            if (res)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("숫자 입력 바람");
+            }
+
+            int x = 0; int y = 0;
+            int vx = 1; int vy = 1;
+            Console.WriteLine("현재 좌표 : (" + x + "," + y + ")");
+            NextPos(x, y, vx, vy, out x, out y);
+        }
+
+        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
         }
     }
 }
