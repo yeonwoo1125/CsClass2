60099460b940499b660e7217e60d9773b2377e0b
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-09-04 17:15:59 +0900

    #6, #7, #8 인터페이스 실습

---
+++ CsClass2/IBasic.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClass2
+{
+    interface IBasic
+    {
+        int TestInstanceMethod();
+        int TestProperty { get; set; }
+    }
+}
